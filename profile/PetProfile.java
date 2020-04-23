8b5e65d175e53c848d7361fa2eb8ccfe39d1d4d6
Emma Griffin <53583563+E-A-Griffin@users.noreply.github.com>
2020-04-23 17:10:06 -0400

    Tabs->Spaces

--- profile/PetProfile.java
+++ profile/PetProfile.java
@@ -15,54 +15,54 @@ import profile.UserProfile;
 public class PetProfile
 {
 
-	public static void editPetInfo()
-	{
-		;
-	}
-	public static void addPetInfo()
-	{
-		String petType, petName;
-		int age;
+  public static void editPetInfo()
+  {
+    ;
+  }
+  public static void addPetInfo()
+  {
+    String petType, petName;
+    int age;
 
-		System.out.print("Please enter your pet's name: ");
-		petName = Validation.getPetName();
+    System.out.print("Please enter your pet's name: ");
+    petName = Validation.getPetName();
 
-		Validation.printPetTypes();
-		System.out.print("Enter what kind of animal your pet is: ");
-		petType = Validation.getPetType();
+    Validation.printPetTypes();
+    System.out.print("Enter what kind of animal your pet is: ");
+    petType = Validation.getPetType();
 
-		System.out.print("How old is your pet? (in years) ");
-		age = Validation.getPetAge();
+    System.out.print("How old is your pet? (in years) ");
+    age = Validation.getPetAge();
 
-		String insertCMD = "INSERT INTO pets (pettype, petname, " +
-			"birthyear, owner)" +
-			"VALUES(" + petType + "', '" + petType + "', age_to_birth_year("
-			+ age + "), '" + Validation.curUsername + "');";
-		Validation.updateSQL(insertCMD);
+    String insertCMD = "INSERT INTO pets (pettype, petname, " +
+      "birthyear, owner)" +
+      "VALUES(" + petType + "', '" + petType + "', age_to_birth_year("
+      + age + "), '" + Validation.curUsername + "');";
+    Validation.updateSQL(insertCMD);
 
-	}
-	public static void displayPetInfo()
-	{
-		try {
-			ResultSet rs
-				= Validation.querySQL("SELECT " +
-															"petname, pettype, " +
-															"birth_year_to_age(birthyear) AS age " +
-															"FROM pets WHERE owner = '"
-															+ Validation.curUsername + "';");
-			if (rs.next()) {
-				System.out.println("petname: " + rs.getString("petname"));
-				System.out.println("pettype: " + rs.getString("pettype"));
-				System.out.println("age: " + rs.getInt("age") + " years");
-				System.out.println();
-			}
-			else
-				System.err.println("Internal error finding entry matching "
-													 + "owner.");
-		}
-		catch (java.sql.SQLException e) {
-			System.err.println(e);
-			System.exit(-1);
-		}
-	}
+  }
+  public static void displayPetInfo()
+  {
+    try {
+      ResultSet rs
+        = Validation.querySQL("SELECT " +
+                              "petname, pettype, " +
+                              "birth_year_to_age(birthyear) AS age " +
+                              "FROM pets WHERE owner = '"
+                              + Validation.curUsername + "';");
+      if (rs.next()) {
+        System.out.println("petname: " + rs.getString("petname"));
+        System.out.println("pettype: " + rs.getString("pettype"));
+        System.out.println("age: " + rs.getInt("age") + " years");
+        System.out.println();
+      }
+      else
+        System.err.println("Internal error finding entry matching "
+                           + "owner.");
+    }
+    catch (java.sql.SQLException e) {
+      System.err.println(e);
+      System.exit(-1);
+    }
+  }
 }
